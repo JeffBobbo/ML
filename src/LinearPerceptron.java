429a5f39ea36f88b093cd397eca438efb6d8bba0
JeffBobbo <james.delph@hotmail.co.uk>
2018-05-05 20:54:23 +0100

    some refactoring

--- src/LinearPerceptron.java
+++ src/LinearPerceptron.java
@@ -2,38 +2,58 @@ import weka.classifiers.AbstractClassifier;
 import weka.core.Instance;
 import weka.core.Instances;
 
-/**
- * Created by james on 05/05/18.
- */
 public class LinearPerceptron extends AbstractClassifier
 {
-  LinearPerceptron()
+  public LinearPerceptron()
   {
-    weightX = -1.0;
-    weightY =  2.0;
+    weightX = 1.0;
+    weightY = 1.0;
+    bias = 0.0;
+  }
+  public LinearPerceptron(double initWeightX, double initWeightY, double b)
+  {
+    weightX = initWeightX;
+    weightY = initWeightY;
+    bias = b;
+  }
+
+  protected double psi(Instance instance)
+  {
+    return weightX * instance.value(0) + weightY * instance.value(1);
   }
 
   @Override
   public void buildClassifier(Instances instances) throws Exception
   {
     boolean fitted = false;
-    while (fitted == false)
+    while (!fitted)
     {
       fitted = true;
       for (Instance instance : instances)
       {
-        double y1 = weightX * instance.value(0) + weightY * instance.value(1);
-        if ((y1 > 0.0 && instance.value(2) < 0.0) || (y1 < 0.0 && instance.value(2) > 0.0))
+        double yi = psi(instance);
+        if ((yi >= 0.0 && instance.value(2) < 0.0) || (yi < 0.0 && instance.value(2) >= 0.0))
         {
           fitted = false;
-          weightX = weightX + 0.5 * LEARNING_RATE * (instance.value(2) - (y1 > 0.0 ? 1.0 : -1.0)) * instance.value(0);
-          weightY = weightY + 0.5 * LEARNING_RATE * (instance.value(2) - (y1 > 0.0 ? 1.0 : -1.0)) * instance.value(1);
+          weightX = weightX + 0.5 * LEARNING_RATE * (instance.value(2) - (yi >= 0.0 ? 1.0 : -1.0)) * instance.value(0) + bias;
+          weightY = weightY + 0.5 * LEARNING_RATE * (instance.value(2) - (yi >= 0.0 ? 1.0 : -1.0)) * instance.value(1) + bias;
         }
       }
     }
   }
 
-  private final double LEARNING_RATE = 1.0;
-  private double weightX;
-  private double weightY;
+  @Override
+  public double classifyInstance(Instance instance)
+  {
+    double y1 = weightX * instance.value(0) + weightY * instance.value(1);
+    return y1 >= 0.0 ? 1.0 : -1.0;
+  }
+
+  public double getWeightX() { return weightX; }
+  public double getWeightY() { return weightY; }
+
+  protected final double LEARNING_RATE = 1.0;
+  protected final double bias;
+  protected double weightX;
+  protected double weightY;
 }
